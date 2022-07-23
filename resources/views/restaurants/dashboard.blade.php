232566d329d30604aa6c256290c5696aba54ad1f
Rizal Kahfi <rizalkahfi25@gmail.com>
2022-07-23 21:37:02 +0700

    reject comment in dashboard page

--- resources/views/restaurants/dashboard.blade.php
+++ resources/views/restaurants/dashboard.blade.php
@@ -4,6 +4,58 @@
     @include('restaurants.notification.expired_notification')
     @include('restaurants.notification.new_order_notification')
     @include('restaurants.notification.call_waiter_notification')
+    <style>
+
+        /* The Modal (background) */
+        .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        padding-top: 100px; /* Location of the box */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+
+        /* Modal Content */
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 36px;
+            border: 1px solid #888;
+            width: 72%;
+            left: 10%;
+        }
+
+        /* The Close Button */
+        .close {
+        color: #aaaaaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+        color: #000;
+        text-decoration: none;
+        cursor: pointer;
+        }
+        
+        .modal-header {
+        padding: 2px 16px;
+        background-color: #f5365c;
+        color: white;
+        }
+        .h2-modal-header{
+        color: white;
+        }
+        .modal-body {padding: 2px 16px;}
+    </style>
     <div class="container-fluid">
 
         <div class="row">
@@ -63,7 +115,7 @@
                                                        onclick="if(confirm('Are you sure you want to accept this Order ?')){ event.preventDefault();document.getElementById('accept_order{{$pending->id}}').submit();}"> {{$selected_language->data['store_accept'] ?? 'Accept'}}</a>
                                                 </div>
                                                 <div class="col">
-                                                    <a class="btn btn-outline-danger btn-sm"
+                                                    <a id="reject-btn-{{$pending->order_unique_id}}" class="btn btn-outline-danger btn-sm"
                                                        onclick="if(confirm('Are you sure you want to reject this Order ?')){ event.preventDefault();document.getElementById('reject_order{{$pending->id}}').submit();}"> {{$selected_language->data['store_reject'] ?? 'Reject'}}</a>
                                                     @endif
                                                 </div>
@@ -81,14 +133,50 @@
                                                     <input style="visibility:hidden" name="status" type="hidden"
                                                            value="2">
                                                 </form>
-                                                <form style="visibility: hidden" method="post"
-                                                      action="{{route('store_admin.update_order_status',['id'=>$pending->id])}}"
-                                                      id="reject_order{{$pending->id}}">
-                                                    @csrf
-                                                    @method('patch')
-                                                    <input style="visibility:hidden" name="status" type="hidden"
-                                                           value="3">
-                                                </form>
+                                                @if($pending->status == 1)
+                                                    <!-- The Modal -->
+                                                    <div id="rejectModal-{{$pending->order_unique_id}}" class="modal">
+
+                                                        <!-- Modal content -->
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h2 class="h2-modal-header">Rejection note</h2>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <form method="post"
+                                                                    action="{{route('store_admin.update_order_status',['id'=>$pending->id])}}"
+                                                                    id="reject_order{{$pending->id}}">
+                                                                    @csrf
+                                                                    @method('patch')
+                                                                    <label>Please provide information if this order is rejected<span style="color:red;">*</span></label>
+                                                                    <textarea name="reject_reason" class="form-control" required></textarea>
+                                                                    <input style="visibility:hidden" name="status" type="hidden" value="3">
+                                                                    <br/>
+                                                                    <button class="btn btn-danger btn-sm text-white" type="submit">
+                                                                        {{$selected_language->data['store_orderstatus_rejectorder'] ?? 'Reject Order'}}
+                                                                    </button>
+                                                                    <button class="btn btn-danger btn-sm text-white close-{{$pending->id}}" type="cancel">
+                                                                        {{$selected_language->data['store_orderstatus_rejectorder'] ?? 'Cancel'}}
+                                                                    </button>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                    <script>
+                                                    document.getElementById("reject-btn-{{$pending->order_unique_id}}").onclick = function() {
+                                                        document.getElementById("rejectModal-{{$pending->order_unique_id}}").style.display = "block";
+                                                    }
+                                                    document.getElementsByClassName("close-{{$pending->id}}")[0].onclick = function() {
+                                                        document.getElementById("rejectModal-{{$pending->order_unique_id}}").style.display = "none";
+                                                    }
+                                                    window.onclick = function(event) {
+                                                    if (event.target == modal) {
+                                                        document.getElementById("rejectModal-{{$pending->order_unique_id}}").style.display = "none";
+                                                    }
+                                                    }
+                                                    </script>
+                                                @endif
                                                 <form style="visibility: hidden" method="post"
                                                       action="{{route('store_admin.update_order_status',['id'=>$pending->id])}}"
                                                       id="complete_order{{$pending->id}}">
